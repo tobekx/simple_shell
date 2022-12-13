e8d45889ca30c1be97743b44210852743dc60c16
tobekx <43343954+tobekx@users.noreply.github.com>
2022-12-13 19:25:41 +0100

    Oluwatobi Simple_shell exercise

---
+++ errors.c
@@ -0,0 +1,80 @@
+#include "shell.h"
+
+/**
+ * _eputs - prints an input string
+ * @str: string to be printed
+ * Return: void
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - writes the char c to stderr
+ * @c: char to print
+ * Return: on success 1
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - writes the char c to a given fd
+ * @c: char to print
+ * @fd: filedescriptor
+ * Return: on success 1
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - prints an input string
+ * @str: string to be printed
+ * @fd: filedescriptor
+ * Return: number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
+
